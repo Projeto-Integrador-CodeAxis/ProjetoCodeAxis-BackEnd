3347775e1ede8714b8cf6d80130f15e6c620f61e
daniele0118 <aheipo@yahoo.com.br>
2024-04-18 14:17:27 -0300

    task 03

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { Categoria } from './categoria/entities/categoria.entities';
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'root',
+    password: '1234',
     database: 'db_codeaxis',
     entities: [Categoria],
     synchronize: true
