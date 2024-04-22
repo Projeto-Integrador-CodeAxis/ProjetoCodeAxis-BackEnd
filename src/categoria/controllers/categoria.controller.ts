84d6074b5769ad24c1fae4e828deeee5e7422ae3
Andre <andrenas.nascimento95@gmail.com>
2024-04-22 11:09:31 -0300

    task04

---
+++ src/categoria/controllers/categoria.controller.ts
@@ -0,0 +1,55 @@
+import { Controller, HttpCode, Get, HttpStatus, Param, ParseIntPipe, Post, Body, Put, Delete } from "@nestjs/common";
+import { CategoriaService } from "../services/categoria.services";
+import { HttpAdapterHost } from "@nestjs/core";
+import { Categoria } from "../entities/categoria.entities";
+
+
+
+
+
+
+@Controller("/categorias")
+export class CategoriaController{
+
+    constructor(private readonly categoriaService: CategoriaService) {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Categoria[]>{
+        return this.categoriaService.findAll();
+
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria>{
+        return this.categoriaService.findById(id);
+
+}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findByCategoria(@Param('categoria') categoria: string): Promise<Categoria[]>{
+        return this.categoriaService.findByCategoria(categoria);
+
+}
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() categoria: Categoria): Promise<Categoria> {
+        return this.categoriaService.create(categoria);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() categoria: Categoria): Promise<Categoria> {
+        return this.categoriaService.update(categoria);
+    }
+    
+    @Delete()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number) {
+           return this.categoriaService.delete(id);
+    }
+
+}
---
+++ src/categoria/services/categoria.services.ts
@@ -0,0 +1,68 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Categoria } from "../entities/categoria.entities";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
+
+
+
+@Injectable()
+export class CategoriaService{
+    constructor(
+        @InjectRepository(Categoria)
+        private categoriaRepository: Repository<Categoria>
+
+    ){}
+
+    async findAll(): Promise<Categoria[]>{
+        return await this.categoriaRepository.find()
+    }
+
+    async findById(id: number): Promise<Categoria>{
+
+        let categoria = await this.categoriaRepository.findOne({
+            where:{
+                id,
+            }
+        });
+
+        if (!categoria)
+            throw new HttpException('Categoria não encontrada!', HttpStatus.NOT_FOUND);
+
+        return categoria;
+        
+    }
+
+        async findByCategoria(categoria: string): Promise<Categoria[]>{
+            return await this.categoriaRepository.find({
+                    where:{
+                        categoria: ILike(`%${categoria}%`)
+                    },
+
+            })
+        }
+
+        async create(categoria: Categoria): Promise<Categoria>{
+            return await this.categoriaRepository.save(categoria);
+        }
+      
+        async update(categoria: Categoria): Promise<Categoria>{
+
+            let buscaCategoria: Categoria = await this.findById(categoria.id);
+
+            if (!buscaCategoria || !categoria.id)
+                throw new HttpException('Categoria não foi encontrada!', HttpStatus.NOT_FOUND)
+
+            return await this.categoriaRepository.save(categoria);
+
+        }
+        
+        async delete(id: number): Promise<DeleteResult>{
+
+            let buscaCategoria: Categoria = await this.findById(id);
+
+            if (!buscaCategoria)
+                throw new HttpException('Categoria não foi encontrado!', HttpStatus.NOT_FOUND)
+            
+            return await this.categoriaRepository.delete(id);
+        }
+}
